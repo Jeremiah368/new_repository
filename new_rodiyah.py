9c9ea871bc8a9160257dc8087b14b82d3d96597d
lemon <bernardjeremiah368@gmail.com>
2024-09-27 12:04:11 +0100

    this is my second project

---
+++ new_rodiyah.py
@@ -0,0 +1,5 @@
+print("my name is rodiyah")
+
+print('ayo is a boy')
+print("abdullahi is 3feet tall and is dark in complexion")
+
---
+++ new_text.py
@@ -0,0 +1,9 @@
+print("what is your name")
+
+name = input()
+
+print("how old are you")
+
+age = input()
+
+print("You will be",str(int(age) + 1), "in next year")
